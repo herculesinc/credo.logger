e3f21304313e6eb755d9e15e0a99188a5c538715
Irakliy <irakliy81@yahoo.com>
2016-07-25 23:12:10 -0700

    minor updates to build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,33 @@
+'use strict';
+// IMPORTS
+// ================================================================================================
 const gulp = require('gulp');
 const del = require('del');
 const exec = require('child_process').exec;
 
+// TASKS
+// ================================================================================================
 // delete bin folder
 gulp.task('clean', function(cb) {
-  del(['bin']).then(paths => cb());
+  del([ 'bin' ]).then(() => { cb(); });
 });
 
 // compile TypeScript files
-gulp.task('build', ['clean'], function (cb) {
-  gulp.src('./package.json').pipe(gulp.dest('./bin'));
-  gulp.src('./credo-logger.d.ts').pipe(gulp.dest('./bin'));
-  gulp.src('../.settings/.npmignore').pipe(gulp.dest('./bin'));
-  gulp.src('./README.md').pipe(gulp.dest('./bin'));
-      
+gulp.task('compile', [ 'clean' ], function (cb) {
   exec('tsc -p .', function (err, stdout, stderr) {
     if (stdout.length > 0) console.log(stdout);
     if (stderr.length > 0) console.error(stderr);
     cb(err);
   });
+});
+
+// build the project
+gulp.task('build', [ 'compile' ], function (cb) {
+  gulp.src('./package.json').pipe(gulp.dest('./bin'));
+  gulp.src('./credo-logger.d.ts').pipe(gulp.dest('./bin'));
+  gulp.src('./.settings/.npmignore').pipe(gulp.dest('./bin'));
+  gulp.src('./README.md').pipe(gulp.dest('./bin'));
+  cb();
 });
 
 // publish to npm
