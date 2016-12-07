03326a2f24de6ceb5a51c682f58d214316ba87ea
Irakliy <irakliy81@yahoo.com>
2016-12-06 17:31:16 -0800

    version bumps

---
+++ bin/test.js
@@ -0,0 +1,32 @@
+"use strict";
+const index_1 = require('./index');
+const options = {
+    name: "dev-logger",
+    log: {
+        messages: true,
+        errors: true,
+        events: true,
+        metrics: true,
+        services: true,
+        requests: true
+    },
+    console: {
+        prefix: false,
+        color: {
+            services: {
+                email: "magenta"
+            }
+        }
+    }
+};
+const logger = new index_1.Logger(options);
+logger.debug('testing');
+logger.trace('email', 'send message', 100, true);
+//logger.increaseIndent();
+logger.debug('testing2');
+//logger.increaseIndent();
+logger.debug('testing3');
+//logger.decreaseIndent();
+//logger.decreaseIndent();
+logger.debug('testing4');
+//# sourceMappingURL=test.js.map
